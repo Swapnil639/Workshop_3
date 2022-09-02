5566290263875f0d38b5b67864fc6dc5a39060aa
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-02 21:11:35 +0530

    Added :Set cards

--- src/com/bridgelabz/DackOfCard.java
+++ src/com/bridgelabz/DackOfCard.java
@@ -3,5 +3,9 @@ package com.bridgelabz;
 public class DackOfCard {
     public static void main(String[] args) {
         System.out.println("Welcome to Deck of Card");
+        String[] suits = {"Club", "Diamond", "Heart", "Spade"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[][] array = new String[4][13];
+
     }
 }
